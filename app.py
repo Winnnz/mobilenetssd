d5b4a7ec30b9e75c93fe36541b724e2bd7308d48
Winnnz <89003916+Winnnz@users.noreply.github.com>
2021-11-26 12:03:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -174,11 +174,11 @@ def event_handle(event,json_line):
             replyObj = TextSendMessage(text=str(response))
             line_bot_api.reply_message(rtoken, replyObj)
         else : 
-           headers = request.headers
-           json_headers = ({k:v for k, v in headers.items()})
-           json_headers.update({'Host':'bots.dialogflow.com'})
-           url = "https://dialogflow.cloud.google.com/v1/integrations/line/webhook/682de730-0e9e-4434-8b48-cc2f6ee918a7"
-           requests.post(url,data=json_line, headers=json_headers)
+            headers = request.headers
+            json_headers = ({k:v for k, v in headers.items()})
+            json_headers.update({'Host':'bots.dialogflow.com'})
+            url = "https://dialogflow.cloud.google.com/v1/integrations/line/webhook/682de730-0e9e-4434-8b48-cc2f6ee918a7"
+            requests.post(url,data=json_line, headers=json_headers)
     elif msgType == "image":
         try:
             message_content = line_bot_api.get_message_content(event['message']['id'])
